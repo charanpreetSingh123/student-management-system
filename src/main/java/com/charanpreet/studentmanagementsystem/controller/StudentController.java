0ff88b95648b797238a05a9585d25ef17c5565f8
Charanpreet <charanpreetsingh627@gmail.com>
2026-04-06 11:56:36 +0530

    Update StudentController

--- src/main/java/com/charanpreet/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/charanpreet/studentmanagementsystem/controller/StudentController.java
@@ -1,12 +1,23 @@
 package com.charanpreet.studentmanagementsystem.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.charanpreet.studentmanagementsystem.entity.Student;
 import com.charanpreet.studentmanagementsystem.service.StudentService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/students")
@@ -17,7 +28,7 @@ public class StudentController {
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
-    }
+    }/*  */
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
